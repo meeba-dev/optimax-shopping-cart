5cda8d58e8a654af162665901488254eb5972a21
meeba-nc <mikhailbaum@icloud.com>
2021-10-07 02:55:23 +0400

    created a react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : []
+    };
+  }
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
+  }
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter( (x) => x._id !== product._id),
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter( (x) => x._id !== product._id)));
+  };
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({cartItems});
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
+  takeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count--;
+        if (item.count === 0) {
+          this.setState({
+            cartItems: cartItems.filter( (x) => x._id !== product._id),
+          });
+        }
+        else {
+          this.setState({cartItems});
+        }
+      }
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter( (x) => x._id !== product._id)));
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">
+            <img src="/images/optimax-logo.png" alt="logo"/>
+          </a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products 
+                products={this.state.products} 
+                addToCart={this.addToCart}>
+              </Products>
+            </div>
+            <div className="sidebar">
+              <Cart createOrder={this.createOrder} cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} addToCart={this.addToCart} takeFromCart={this.takeFromCart}/>
+            </div>
+          </div>
+        </main>
+        <footer>
+            <p> Made by Mikhail Baum &#9752;</p>
+            <p>&copy; All copyrights reserved 2021</p>
+        </footer>
+      </div>
+      );
+  }
 }
 
 export default App;
